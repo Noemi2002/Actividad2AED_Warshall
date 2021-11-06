d63dde8422310234fefbb71cc88fe6eb36ec4f33
3derv <ederv1067@gmail.com>
2021-11-06 15:45:42 -0600

    Solución de error y documentación

--- src/WarshallTrans.java
+++ src/WarshallTrans.java
@@ -1,24 +1,36 @@
 public class WarshallTrans {
+    //Algoritmo de Warshall con cierre transitivo, recibe una matriz común y devuelve una matriz transitiva
     public int[][] AlgWarshallTrans(int[][] matriz) {
-
+        //Se recorren lo elementos de la matriz
         for (int k = 0; k < matriz.length; k++) {
-            for (int i = 0; i < matriz.length; i++) {
-                for (int j = 0; j < matriz.length; j++) {
-                    if (i == k || j == k) continue;
-                    if (1 == matriz[k][i] && matriz[j][k] == 1) {
-                        matriz[i][j] = 1;
+            //Se recorren las filas de la matriz
+            for (int f = 0; f < matriz.length; f++) {
+                //Se recorren las columnas de la matriz
+                for (int c = 0; c < matriz.length; c++) {
+                    //Entra a las validaciones
+                    //Si la fila o la columna pertenecen a la fila u columna que se está evaluando se ignora
+                    if (f == k || c == k) continue;
+                    //Si dos elementos de la lista coinciden en tener ambos un valor de 1, entonces se coloca 1 en la intersección de estos
+                    if (1 == matriz[k][f] && matriz[c][k] == 1) {
+                        matriz[c][f] = 1;
                     }
                 }
             }
 
         }
+        //Se devuelve la matriz transitiva
         return matriz;
     }
+    //Metodo para imprimir los datos de las matrices en consola
     public void ImprimirMatriz(int[][] matriz){
         for (int[] elementos : matriz) {
             System.out.print("[");
             for (int c = 0; c < matriz.length; c++) {
-                System.out.print(elementos[c] + ",");
+                if (c == matriz.length - 1) {
+                    System.out.print(elementos[c]);
+                } else {
+                    System.out.print(elementos[c] + ",");
+                }
             }
             System.out.println("]");
         }
